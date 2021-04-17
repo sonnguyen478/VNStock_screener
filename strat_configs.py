9f786ed3678cf1cc944908e8804d2004e041811a
sonnguyen-TS <son.nguyen@trustingsocial.com>
2021-04-17 18:31:36 +0700

    Remove slack token

--- strat_configs.py
+++ strat_configs.py
@@ -7,6 +7,3 @@ fconf['fav_lst'] = ["VNM", "HPG", "FPT", "GEX", "GTN", "VLC",
 
 # SLACK config
 fconf['slack_channel'] = 'vnindex-update'
-fconf['slack_token']= 'xoxb-1849780713382-1849890548022-g09pgXvwEe68fuHV5rvoBDbG'
-#xoxb-1849780713382-1849890548022-1Z8LFPBASMfLenSjNjlebAAI
-#xoxp-1849780713382-1853470688245-1856881687203-5a2f28f6d44af791a6086beb9089dd32
